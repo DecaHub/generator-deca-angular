091984c496ea8ff7823360d8e5f41ba335387585
DecaHub <dan@decahub.com>
2017-03-12 08:48:14 -0400

    Improve formatting of index.js and strengthen syntax.

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,99 +1,109 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+"use strict";
+
+const yeoman = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 const path = require("path");
-const mkdirp = require('mkdirp');
+const mkdirp = require("mkdirp");
 
 module.exports = yeoman.generators.Base.extend({
-	//Configurations will be loaded here.
-	//Ask for user input
-	prompting: function() {
-		var done = this.async();
+	// Configurations will be loaded here.
+	// Ask for user input
+	prompting () {
+		
+		const done = this.async();
+		
 		this.prompt([
 			{
-				type: 'input',
-				name: 'name',
-				message: 'Your project name: ',
-				//Defaults to the project's folder name if the input is skipped
-				default: this.appname.replace(/\s+/g, '-')
+				type: "input",
+				name: "name",
+				message: "Your project name: ",
+				// Defaults to the project"s folder name if the input is skipped
+				default: this.appname.replace(/\s+/g, "-")
 			},
 			{
-				type: 'input',
-				name: 'author',
-				message: 'Your name or organization: ',
+				type: "input",
+				name: "author",
+				message: "Your name or organization: "
 			}
-			], function(answers) {
+		], function (answers) {
+			
 			this.props = answers;
 			done();
+		
 		}.bind(this));
+	
 	},
-	//Writing Logic here
+	// Writing Logic here
 	writing: {
-		//Copy the configuration files
-		config: function() {
+		// Copy the configuration files
+		config () {
+			
 			this.fs.copyTpl(
-				this.templatePath('_package.json'),
-				this.destinationPath('package.json'), {
+				this.templatePath("_package.json"),
+				this.destinationPath("package.json"), {
 					name: this.props.name,
 					author: this.props.author
 				}
 			);
 			this.fs.copyTpl(
-				this.templatePath('_bower.json'),
-				this.destinationPath('bower.json'), {
+				this.templatePath("_bower.json"),
+				this.destinationPath("bower.json"), {
 					name: this.props.name,
-					author: this.props.author,
+					author: this.props.author
 				}
 			);
 			this.fs.copyTpl(
-				this.templatePath('README.md'),
-				this.destinationPath('README.md'), {
+				this.templatePath("README.md"),
+				this.destinationPath("README.md"), {
 					name: this.props.name,
-					author: this.props.author,
+					author: this.props.author
 				}
 			);
 			
 			
 			// Configuration Files
 			this.fs.copy(
-				this.templatePath('.babelrc'),
-				this.destinationPath('.babelrc')
-			);
-      this.fs.copy(
-				this.templatePath('.bowerrc'),
-				this.destinationPath('.bowerrc')
-			);
-      this.fs.copy(
-        this.templatePath('.eslintrc.json'),
-        this.destinationPath('.eslintrc.json')
-      );
-      this.fs.copy(
-				this.templatePath('.gitignore'),
-				this.destinationPath('.gitignore')
+				this.templatePath(".babelrc"),
+				this.destinationPath(".babelrc")
 			);
 			this.fs.copy(
-				this.templatePath('Gulpfile.js'),
-				this.destinationPath('Gulpfile.js')
+				this.templatePath(".bowerrc"),
+				this.destinationPath(".bowerrc")
 			);
+			this.fs.copy(
+				this.templatePath(".eslintrc.json"),
+				this.destinationPath(".eslintrc.json")
+			);
+			this.fs.copy(
+				this.templatePath(".gitignore"),
+				this.destinationPath(".gitignore")
+			);
+			this.fs.copy(
+				this.templatePath("Gulpfile.js"),
+				this.destinationPath("Gulpfile.js")
+			);
+		
 		},
 
-		//Copy application files
-		app: function() {
-			//Server file
+		// Copy application files
+		app () {
+			
+			// Server file
 			this.fs.copy(
-				this.templatePath('app'),
-				this.destinationPath('app')
+				this.templatePath("app"),
+				this.destinationPath("app")
 			);
 			
 			this.fs.copyTpl(
-				this.templatePath('app/index.html'),
-				this.destinationPath('app/index.html'), {
-					name: this.props.name
-				}
+				this.templatePath("app/index.html"),
+				this.destinationPath("app/index.html"), {name: this.props.name}
 			);
 			
-			mkdirp.sync(path.join('app', 'dist'));
+			mkdirp.sync(path.join("app", "dist"));
+		
 		}
+		
 	}
+	
 });
